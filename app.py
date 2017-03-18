71032104a126bd5725192b6ac5dd32a876c9ebf1
swathijasti <swathi.lbs16@gmail.com>
2017-03-17 21:18:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
 from flask import Flask
+from github import Github
+import base64
 import sys
 import yaml
 import json
-from github3 import GitHub
-import base64
 
 app = Flask(__name__)
 
-g=GitHub()
-shettyaditi=g.user('shettyaditi')
-
-username = sys.argv[1].split("/", 4)[3]
-repository = sys.argv[1].split("/", 4)[4]
-
+g=Github()
+argument=sys.argv[1].split("/")
+username = argument[len(argument)-2]
+repository = argument[len(argument)-1]
+repo=g.get_user(username).get_repo(repository)
 @app.route("/v1/<filename>")
 def hello1(filename):
     if filename.endswith('.yml'):
-        r=base64.b64decode(g.repository(username,repository).contents(filename).content)
+        r=base64.b64decode(repo.get_file_contents(filename).content)
         return r
 
     if filename.endswith('.json'):
-        i=base64.b64decode(g.repository(username,repository).contents(filename[:-5]+".yml").content)
+        i=base64.b64decode(repo.get_file_contents(filename[:-5]+".yml").content)
         out=(json.dumps(yaml.load(i), sort_keys=True, indent=2))
         return out
 
